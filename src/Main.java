c9b3c2057135ec587f6c9b8680ecf6dcd24a9818
Rigoberto <rigoma018@gmail.com>
2025-10-23 16:35:00 -0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    /**
+     *
+     * @param args
+     */
+    void main(String[] args) {
+// tipo de datos primitivos en java
+
+        byte edad = 25;
+        System.out.println("bite edad = " + edad);
+
+        short ano = 2025;
+        System.out.println("short ano = " + ano);
+
+        int poblacionCiudad = 15000000;
+        System.out.println("int pablacionMundia = " + poblacionCiudad);
+
+        long poblacionMundial = 8_000_000_000L;
+        System.out.println("long pablacion mundia = " + poblacionMundial);
+
+        float precio = 99.99f;
+        System.out.println("float precio = " + precio);
+
+        double pi = 2.141592653589793;
+        System.out.println("double pi = " + pi);
+
+        char inicial = 'C';
+        System.out.println("char inicial = " + inicial);
+
+        
+    }
+}
